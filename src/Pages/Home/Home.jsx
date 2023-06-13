ecbfce250175462c2316e7179cbb35b28a471472
AlaminHasanPro <alaminhasanpro@gmail.com>
2023-06-13 22:55:48 +0600

    file structure added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,12 @@
-
+import NavigationBar from "../Shared/NavigationBar";
 
 const Home = () => {
-    return (
-        <div>
-            <h2>I am a Web Developer</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2>I am a Web Developer</h2>
+      <NavigationBar />
+    </div>
+  );
 };
 
 export default Home;
